f91edd0d079bb2ff0819ab33e42dfcad79745237
srikar26 <m.k.saisrikar2002@gmail.com>
2022-01-02 00:28:07 +0530

    Add files via upload
    Few Math module funtions

---
+++ math.py
@@ -0,0 +1,9 @@
+import math
+a=float(input())
+print(math.factorial(int(a)))  #returns factorial of a
+print(round(a)) #returns round of a
+print(math.ceil(a)) # returns round up
+print(math.floor(a)) #returns round down
+print(math.pow(3,2)) #return square of  number
+print(math.sqrt(25)) #returns square root of number
+print(math.gcd(25,30)) #returns gcd of 2 numbers 
